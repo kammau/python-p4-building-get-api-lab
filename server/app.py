6bf7e4304f7dab751cc3dbb2ec0684c1de7990ce
Kamryn Smith <programersign@gmail.com>
2023-08-03 15:13:28 -0700

    assignment complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,53 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakes = Bakery.query.all()
+    serialized_bakeries = [bakery.to_dict() for bakery in bakes]
+
+    response = make_response(jsonify(serialized_bakeries), 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    serialized_bakery = bakery.to_dict()
+
+    response = make_response(
+        jsonify(serialized_bakeries),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    serialized_goods = [baked_good.to_dict() for baked_good in baked_goods_by_price]
+
+    response = make_response(
+        jsonify(serialized_goods),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    me_baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    serialized_me = me_baked_goods.to_dict()
+
+    response = make_response(
+        jsonify(serialized_me),
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
